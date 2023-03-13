404588385ad85a9238519d6b9cca88398524c2bb
Chuks-project <chuksy237@yahoo.com>
2023-03-13 12:27:39 +0000

    update

--- main.tf
+++ main.tf
@@ -1,72 +1,35 @@
 
 provider "aws" {
-  region = "eu-west-1"
-} variable "region" {
-        default = "eu-central-1"
-    }
-
-    variable "vpc_cidr" {
-        default = "172.16.0.0/16"
-    }
-
-    variable "enable_dns_support" {
-        default = "true"
-    }
-
-    variable "enable_dns_hostnames" {
-        default ="true" 
-    }
-
-    variable "enable_classiclink" {
-        default = "false"
-    }
-
-    variable "enable_classiclink_dns_support" {
-        default = "false"
-    }
-
-    provider "aws" {
-    region = var.region
-    }
-
-    # Create VPC
-    resource "aws_vpc" "main" {
-    cidr_block                     = var.vpc_cidr
-    enable_dns_support             = var.enable_dns_support 
-    enable_dns_hostnames           = var.enable_dns_support
-    enable_classiclink             = var.enable_classiclink
-    enable_classiclink_dns_support = var.enable_classiclink
-
-    }
-
-        # Get list of availability zones
-        data "aws_availability_zones" "available" {
-        state = "available"
-        }
+ region = var.region
+}
 
 # Create VPC
 resource "aws_vpc" "main" {
-  cidr_block                     = "172.16.0.0/16"
-  enable_dns_support             = "true"
-  enable_dns_hostnames           = "true"
-  enable_classiclink             = "false"
-  enable_classiclink_dns_support = "false"
+  cidr_block                     = var.vpc_cidr
+  enable_dns_support             = var.enable_dns_support
+  enable_dns_hostnames           = var.enable_dns_hostnames
+  enable_classiclink             = var.enable_classiclink
+  enable_classiclink_dns_support = var.enable_classiclink_dns_support
 }
 
-        # Get list of availability zones
-        data "aws_availability_zones" "available" {
-        state = "available"
-        }
+ 
+# Get list of availability zones
+   data "aws_availability_zones" "available" {
+    state = "available"
+  }
+ 
+    # Create public subnets
+resource "aws_subnet" "public" {
+  count  = var.preferred_number_of_public_subnets == null ? length(data.aws_availability_zones.available.names) : var.preferred_number_of_public_subnets   
+  vpc_id = aws_vpc.main.id
+  cidr_block              = cidrsubnet(var.vpc_cidr, 4 , count.index)
+  map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
 
-#    # Create public subnet1
-    resource "aws_subnet" "public" { 
-        count                   = 2
-        vpc_id                  = aws_vpc.main.id
-        cidr_block              = "172.16.1.0/24"
-        map_public_ip_on_launch = true
-        availability_zone       = data.aws_availability_zones.available.names[count.index]
+}
 
-    }
+       
+   
 
 
 
